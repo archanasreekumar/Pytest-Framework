0efc31cc3a41a930ac1b74e3edbf028382cbecc6
archanasreekumar <sarchanasreekumar@gmail.com>
2026-04-05 22:03:52 +0530

    Added logging

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,9 @@
+from utils.logger import get_logger
 
+logger = get_logger()
 
 def test_get_users(api_client_fixture):
+    logger.info("Calling API")
     response = api_client_fixture("/users")
     assert response.status_code == 200
 
